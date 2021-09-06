6cff47ba7def01e6f0335f9c41c1e703075c8258
Tomek <61616164+masa87@users.noreply.github.com>
2021-09-06 22:03:15 +0200

    update package.json url's

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import _default from '../../node_modules/@vimeo/player';
 
+let iframe = document.querySelector('iframe');
+let player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', function (duration) {
+  console.log(duration.seconds);
+  localStorage.setItem('videoplayer-current-time', duration.seconds);
+});
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
