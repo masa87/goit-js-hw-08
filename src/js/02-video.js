a913bbd63bf0d85a5fe30eed07a828c7a509001a
Tomek <61616164+masa87@users.noreply.github.com>
2021-09-07 19:00:28 +0200

    task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,8 @@
 import _default from '../../node_modules/@vimeo/player';
+var throttle = require('lodash.throttle');
 
-let iframe = document.querySelector('iframe');
-let player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
 player.on('play', function () {
   console.log('played the video!');
@@ -11,24 +12,12 @@ player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
 
-player.on('timeupdate', function (duration) {
-  console.log(duration.seconds);
-  localStorage.setItem('videoplayer-current-time', duration.seconds);
-});
+player.on(
+  'timeupdate',
+  throttle(event => {
+    console.log(event.seconds);
+    localStorage.setItem('videoplayer-current-time', `${event.seconds}`);
+  }, 1000),
+);
 
-player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
